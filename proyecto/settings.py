49f50c022daf20af9799c52515148e2396d0cc70
jaivalos-dev <jaivalos.dev@gmail.com>
2024-03-16 11:59:29 -0600

    Actualizaciones

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -55,7 +55,12 @@ ROOT_URLCONF = 'proyecto.urls'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'staticfiles/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
 
 SITE_ID = 1
 
@@ -131,11 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
