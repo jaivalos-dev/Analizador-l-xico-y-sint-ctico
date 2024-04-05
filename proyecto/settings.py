d2b60b702978e2b8d7ece163700e4f4e5bb27eae
Javier Alejandro Avalos Galindo <152330639+jaivalos-dev@users.noreply.github.com>
2024-04-04 22:22:13 -0600

    Update settings allowed hosts.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-1#h=8^($$g4rflp=bxme#=130sb!8n$9l#g8h2atf*o^-g&nxo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
