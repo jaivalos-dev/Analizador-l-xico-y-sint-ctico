419605f2082d5bc304acfb83f9e9538eed098119
jaivalos-dev <jaivalos.dev@gmail.com>
2024-05-08 21:45:04 -0600

    Fase 2 casi terminada

--- staticfiles/lexico.js
+++ staticfiles/lexico.js
@@ -112,8 +112,8 @@ const expresionesRegulares = {
   //Identificadores
   identificadores: /^[a-zA-Z_]\w*$/,
 
-  //comentario
-  comentario: /^(")([a-zA-Z_0-9])(")$/,
+  //string
+  string: /^\"[a-zA-Z_ 0-9]*\"$/,
 };
 
 function lexico(input) {
@@ -124,19 +124,19 @@ function lexico(input) {
   const tokens = input
     .replace(/\s+/g, "") // Eliminar espacios en blanco
     .match(
-        /(\d+|(")([a-zA-Z_0-9])(")|\*\*|\-\-|\/\-|\+\=|\+|\-\=|\-|(\*args)|(\*\*kwargs)|\*|\/|\(|\)|\^|\"|\!|\¡|\@|\#|\%|\&|\=|\[|\]|\{|\}|\\|\||\:|\;|\'|\<\=|\>\=|\<|\>|\.|\,|\?|\¿|(clesszy)|(difzy)|(__onotzy__)|(silfzy)|(wholizy)|(prontzy)|(furzy)|(in)|(du)|(ontzy)|(rtszy)|(fluetzy)|(cumplixzy)|(lostzy)|(taplizy)|(sitzy)|(doctzy)|(__niwzy__)|(super)|(clszy)|(ritarnzy)|(traplizy)|(__cellzy__)|(ompatzy)|(pesszy)|(endzy)|(ompurtzy)|(briekzy)|(rengizy)|(dilzy)|(ixciptzy)|(troyzy)|(eszy)|(fonellyzy)|(frumzy)|(felsizy)|(strongzy)|(rew-strongzy)|(fstrongzy)|(nunizy)|(traizy)|(ofzy)|(ilofzy)|(ilsizy)|(cuntonaizy)|[a-zA-Z_]\w*|\_|\S)/g
+        /(\d+|\"[a-zA-Z_ 0-9]*\"|\*\*|\-\-|\/\-|\+\=|\+|\-\=|\-|(\*args)|(\*\*kwargs)|\*|\/|\(|\)|\^|\"|\!|\¡|\@|\#|\%|\&|\=|\[|\]|\{|\}|\\|\||\:|\;|\'|\<\=|\>\=|\<|\>|\.|\,|\?|\¿|(clesszy)|(difzy)|(__onotzy__)|(silfzy)|(wholizy)|(prontzy)|(furzy)|(in)|(du)|(ontzy)|(rtszy)|(fluetzy)|(cumplixzy)|(lostzy)|(taplizy)|(sitzy)|(doctzy)|(__niwzy__)|(super)|(clszy)|(ritarnzy)|(traplizy)|(__cellzy__)|(ompatzy)|(pesszy)|(endzy)|(ompurtzy)|(briekzy)|(rengizy)|(dilzy)|(ixciptzy)|(troyzy)|(eszy)|(fonellyzy)|(frumzy)|(felsizy)|(strongzy)|(rew-strongzy)|(fstrongzy)|(nunizy)|(traizy)|(ofzy)|(ilofzy)|(ilsizy)|(cuntonaizy)|[a-zA-Z_]\w*|\_|\S)/g
       ); // Identificar números, operadores, identificadores y otros caracteres
   // Dividir cada línea en tokens y registrar el número de línea para cada token
   const tokensWithLine = lines.flatMap((line, lineNumber) => {
-    return line.match(/(\d+|\*\*|\-\-|\/\-|\+\=|\+|\-\=|\-|(\*args)|(\*\*kwargs)|\*|\/|\(|\)|\^|\"|\!|\¡|\@|\#|\%|\&|\=|\[|\]|\{|\}|\\|\||\:|\;|\'|\<\=|\>\=|\<|\>|\.|\,|\?|\¿|(clesszy)|(difzy)|(__onotzy__)|(silfzy)|(wholizy)|(prontzy)|(furzy)|(in)|(du)|(ontzy)|(rtszy)|(fluetzy)|(cumplixzy)|(lostzy)|(taplizy)|(sitzy)|(doctzy)|(__niwzy__)|(super)|(clszy)|(ritarnzy)|(traplizy)|(__cellzy__)|(ompatzy)|(pesszy)|(endzy)|(ompurtzy)|(briekzy)|(rengizy)|(dilzy)|(ixciptzy)|(troyzy)|(eszy)|(fonellyzy)|(frumzy)|(felsizy)|(strongzy)|(rew-strongzy)|(fstrongzy)|(nunizy)|(traizy)|(ofzy)|(ilofzy)|(ilsizy)|(cuntonaizy)|[a-zA-Z_]\w*|\_|\S)/g).map(token => ({ token, lineNumber: lineNumber + 1 }));
+    return line.match(/(\d+|\"[a-zA-Z_ 0-9]*\"|\*\*|\-\-|\/\-|\+\=|\+|\-\=|\-|(\*args)|(\*\*kwargs)|\*|\/|\(|\)|\^|\"|\!|\¡|\@|\#|\%|\&|\=|\[|\]|\{|\}|\\|\||\:|\;|\'|\<\=|\>\=|\<|\>|\.|\,|\?|\¿|(clesszy)|(difzy)|(__onotzy__)|(silfzy)|(wholizy)|(prontzy)|(furzy)|(in)|(du)|(ontzy)|(rtszy)|(fluetzy)|(cumplixzy)|(lostzy)|(taplizy)|(sitzy)|(doctzy)|(__niwzy__)|(super)|(clszy)|(ritarnzy)|(traplizy)|(__cellzy__)|(ompatzy)|(pesszy)|(endzy)|(ompurtzy)|(briekzy)|(rengizy)|(dilzy)|(ixciptzy)|(troyzy)|(eszy)|(fonellyzy)|(frumzy)|(felsizy)|(strongzy)|(rew-strongzy)|(fstrongzy)|(nunizy)|(traizy)|(ofzy)|(ilofzy)|(ilsizy)|(cuntonaizy)|[a-zA-Z_]\w*|\_|\S)/g).map(token => ({ token, lineNumber: lineNumber + 1 }));
   });
 
   // Identificar el tipo de cada token y retornarlos
   const tokenTypes = tokensWithLine.map(({ token, lineNumber }) => {
     if (expresionesRegulares.numeros.test(token)) {
       return { type: "t_num", value: parseInt(token, 10), lineNumber  }; // Número
-    } else if (expresionesRegulares.comentario.test(token)) {
-      return { type: "t_comentario", value: token, lineNumber }; // Comentario
+    } else if (expresionesRegulares.string.test(token)) {
+      return { type: "t_string", value: token, lineNumber }; // Comentario
     } else if (expresionesRegulares.asignacion.test(token)) {
       return { type: "t_asignacion", value: token, lineNumber }; // Operador de asignación
     } else if (expresionesRegulares.suma.test(token)) {
